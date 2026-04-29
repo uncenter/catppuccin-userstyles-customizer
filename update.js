e8e0a8f5f2cb4c0a085534f5675c7ea3b1ba1ca0
uncenter <uncenter@uncenter.dev>
2026-04-29 09:55:53 -0500

    fix: fetch release asset using authenticated octokit

--- update.js
+++ update.js
@@ -13,16 +13,15 @@ const release = await github.rest.repos.getReleaseByTag({
   tag: 'all-userstyles-export',
 })
 const id = release.data.assets.find((asset) => asset.name === 'import.json').id;
-const url = `https://api.github.com/repos/catppuccin/userstyles/releases/assets/${id}`;
 
-fetch(url, {
-  method: "GET",
-  headers: {
-    "Accept": "application/octet-stream"
-  }
+const asset = await github.rest.repos.getReleaseAsset({
+	owner: 'catppuccin',
+	repo: 'userstyles',
+	asset_id: id,
+	headers: {
+  		"Accept": "application/octet-stream"
+	}
 })
-  .then(response => response.arrayBuffer())
-  .then(buffer => {
-    const outpath = path.join(process.cwd(), "import.json");
-    fs.writeFileSync(outpath, Buffer.from(buffer));
-  });
+
+const outpath = path.join(process.cwd(), "import.json");
+fs.writeFileSync(outpath, Buffer.from(asset.data));
