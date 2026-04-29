6376be559862f669d7be4960771edd0349e0a72a
uncenter <uncenter@uncenter.dev>
2026-04-29 09:57:46 -0500

    style: format with prettier

--- update.js
+++ update.js
@@ -4,14 +4,14 @@ import fs from 'fs';
 import path from 'path';
 
 const github = new Octokit({
-  auth: process.env.GITHUB_TOKEN,
+	auth: process.env.GITHUB_TOKEN,
 });
 
 const release = await github.rest.repos.getReleaseByTag({
-  owner: 'catppuccin',
-  repo: 'userstyles',
-  tag: 'all-userstyles-export',
-})
+	owner: 'catppuccin',
+	repo: 'userstyles',
+	tag: 'all-userstyles-export',
+});
 const id = release.data.assets.find((asset) => asset.name === 'import.json').id;
 
 const asset = await github.rest.repos.getReleaseAsset({
@@ -19,9 +19,9 @@ const asset = await github.rest.repos.getReleaseAsset({
 	repo: 'userstyles',
 	asset_id: id,
 	headers: {
-  		"Accept": "application/octet-stream"
-	}
-})
+		Accept: 'application/octet-stream',
+	},
+});
 
-const outpath = path.join(process.cwd(), "import.json");
+const outpath = path.join(process.cwd(), 'import.json');
 fs.writeFileSync(outpath, Buffer.from(asset.data));
